f19e61f7b4e86345ca21615ceaf2c7057c2c2848
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-09-09 18:09:50 +0530

    Initial Commit

---
+++ DailyRound/DailyRoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyRoundApp.swift
+//  DailyRound
+//
+//  Created by Piyush saini on 09/09/24.
+//
+
+import SwiftUI
+
+@main
+struct DailyRoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
